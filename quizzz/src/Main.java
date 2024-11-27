26ca00de97afe01b0f23bf707c5b17198e7498ce
medhai243 <medha.iyer07@gmail.com>
2024-11-27 10:30:46 -0500

    Created lambda expression
    Created lambda expression to find the sum of numbers in a range. Uses command line/terminal.

--- quizzz/src/Main.java
+++ quizzz/src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args)
     {
+        Addition add = (num1,num2) -> {
+            int sum = 0;
+            for(int i = num1; i <= num2; i++)
+            {
+                sum+=i;
+            }
+            return sum;
+        };
 
-        System.out.println("Hello, World!");
+        System.out.println(add.addNumsinRange(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
     }
 }
