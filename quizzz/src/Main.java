04b6fc06c01431f95326330e113326b6459ee860
svel430 <sriyaveluri@gmail.com>
2024-11-27 10:39:39 -0500

    comment in main

--- quizzz/src/Main.java
+++ quizzz/src/Main.java
@@ -2,6 +2,9 @@
 // Partner B; Medha Iyer
 
 public class Main {
+    // Partner A: Sriya Veluri
+
+    // Partner B; Medha Iyer
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
